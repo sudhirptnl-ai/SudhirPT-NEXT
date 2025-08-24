5944c1c365314e2834f3d995d6d17107da1ec812
sudhirptnl-ai <sudhirptnl@gmail.com>
2025-08-24 19:48:03 +0200

    refactor: unify Sanity client and fix sitemap query

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,49 +1,31 @@
 // pages/sitemap.xml.js
-import { groq } from "next-sanity";
-import { getClient } from "../lib/sanity.server"; // pas aan naar jouw sanity client pad
+import { serverClient } from "../lib/client";
 
-const query = groq`*[_type == "post"]{ "slug": slug.current, publishedAt }`;
-
-function generateSiteMap(posts) {
-  const baseUrl = "https://www.sudhirpt.nl"; // ✅ pas aan naar je domein
-
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!-- Statische pagina’s -->
-     <url><loc>${baseUrl}/</loc></url>
-     <url><loc>${baseUrl}/over</loc></url>
-     <url><loc>${baseUrl}/locaties</loc></url>
-     <url><loc>${baseUrl}/diensten</loc></url>
-     <url><loc>${baseUrl}/tarieven</loc></url>
-     <url><loc>${baseUrl}/contact</loc></url>
-     <url><loc>${baseUrl}/blog</loc></url>
-
-     <!-- Blogposts -->
-     ${posts
-       .map((post) => {
-         return `
-           <url>
-             <loc>${baseUrl}/blog/${post.slug}</loc>
-             <lastmod>${post.publishedAt || new Date().toISOString()}</lastmod>
-           </url>
-         `;
-       })
-       .join("")}
-   </urlset>
- `;
-}
+const query = `*[_type == "post"]{ "slug": slug.current, publishedAt }`;
 
 export async function getServerSideProps({ res }) {
-  const posts = await getClient().fetch(query);
-  const sitemap = generateSiteMap(posts);
+  const posts = await serverClient.fetch(query);
 
   res.setHeader("Content-Type", "text/xml");
+  const sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${posts
+        .map(
+          (post) => `
+        <url>
+          <loc>https://sudhirpt.nl/posts/${post.slug}</loc>
+          <lastmod>${post.publishedAt}</lastmod>
+        </url>`
+        )
+        .join("")}
+    </urlset>
+  `;
   res.write(sitemap);
   res.end();
 
   return { props: {} };
 }
 
-export default function SiteMap() {
+export default function Sitemap() {
   return null;
 }
